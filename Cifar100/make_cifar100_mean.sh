0d23ba586bdc488c489bb0ad6324c09e0c082a92
Md Amir Sohail <mdas442006@gmail.com>
2018-07-05 23:10:14 +0530

    Create mean.binaryproto of CIFAR100

---
+++ Cifar100/make_cifar100_mean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# Compute the mean image from the cifar100 training lmdb
+
+
+EXAMPLE=/media/path/to/datasets/ # Location of cifar100_train_lmdb
+DATA=/media/path/to/datasets/ # Where to store the mean.binaryproto
+TOOLS=build/tools # Location of compute_image_mean in this folder
+
+$TOOLS/compute_image_mean $EXAMPLE/cifar100_train_lmdb \
+  $DATA/cifar100_mean.binaryproto
+
+echo "cifar100_mean.binaryproto file generation is done."
